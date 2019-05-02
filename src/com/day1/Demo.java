e6b55e3c50d33fb15ad7504d5d152ccbc414553f
vishu <vishu0487@gmail.com>
2019-05-03 03:43:09 +0530

    test

--- src/com/day1/Demo.java
+++ src/com/day1/Demo.java
@@ -1,10 +1,46 @@
 package com.day1;
-
+/*
 public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("HEllo");
+
+
+
+
 	}
+	
+	
+	
+	
 
 }
+*/
+/*
+ * class A{ void msg(){System.out.println("Hello");} } class B{ void
+ * msg(){System.out.println("Welcome");} } class C extends A{//suppose if it
+ * were
+ * 
+ * public static void main(String args[]){ C obj=new C(); obj.msg();//Now which
+ * msg() method would be invoked? } }
+ */
+
+
+
+class Animal{  
+void bark(){System.out.println("eating...");}  
+}  
+class Dog extends Animal{  
+void bark(){System.out.println("barking...");}  
+}  
+class Cat extends Animal{  
+void meow(){System.out.println("meowing...");}  
+}  
+class Demo{  
+public static void main(String args[]){  
+Cat c=new Cat();  
+c.meow();  
+c.bark();  
+//c.bark();//C.T.Error  
+}}  
---
+++ src/com/day2/Demo.java
@@ -0,0 +1,14 @@
+package com.day2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver wD = new ChromeDriver();
+
+	}
+
+}
